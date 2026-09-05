2338371309df4f2ac5a1f57298a5071cfc5b8714
Nia Myers <daniamyers0@gmail.com>
2026-09-05 08:53:17 -0400

    I matched the right candidate to the right job

--- 8-kyu/main.js
+++ 8-kyu/main.js
@@ -1,1 +1,16 @@
-alert(works)
+let candidate = {
+  minSalary: 100000
+}
+let job = {
+  maxSalary:350000
+}
+
+function match(candidate, job) {
+  // is this job a valid match for the candidate?
+   if (!candidate || !job || candidate.minSalary === undefined || job.maxSalary === undefined) {
+    throw new Error("Missing salary information");
+  }
+  
+  // 2. Candidate minSalary gets a 10% "wiggle room" discount (multiplied by 0.9)
+  return (candidate.minSalary * 0.9) <= job.maxSalary;
+}
